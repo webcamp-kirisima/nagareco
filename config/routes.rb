5ff1f6af938e2512bfb85b15e087651dc8d67dcb
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-07-13 15:03:56 +0900

    T-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :orders
   resources :users
   resources :cart_items
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
   resources :admin_order
   resources :admin_products
   resources :admin_users
-  devise_for :users
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
