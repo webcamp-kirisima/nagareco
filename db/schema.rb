bcc1d11aa961207511966fd5480327c4a40d02b9
kurara0327 <h.kurara080327@gmail.com>
2019-07-14 16:09:58 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_07_14_062528) do
-=======
-ActiveRecord::Schema.define(version: 2019_07_14_054054) do
->>>>>>> develop
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
