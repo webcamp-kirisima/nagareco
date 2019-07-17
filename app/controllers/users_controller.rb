4a59ac46a5c93e7603cb767baf25fa49a9a2fef4
nishi <24yuta24@gmail.com>
2019-07-17 15:31:32 +0900

    [ユーザー機能完了]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
 
   def destroy
      user = User.find(params[:id])
-     user.destroy
-
+     if user.destroy
+        redirect_to root_path
   end
 
   private
