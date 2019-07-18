91eb6dea917499e4d2d363967f39eec05c429559
新井　雄介 <ay088055@gmail.com>
2019-07-18 19:56:22 +0900

    A-index-6

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-  	@products = products.find(params[:id])
+  	@product = products.find(params[:id])
   end
 end
 
@@ -12,3 +12,4 @@ private
   def products_params
     params.require(:product).permit(:artist_id, :cd_title, :image, :notax_price, :label, :genre, :stock )
   end
+
