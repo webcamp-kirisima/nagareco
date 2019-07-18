564379842997904e2522a466c297d3238191303b
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-07-18 11:38:07 +0900

    5893wd

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,34 @@
 class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+    @product.discs.build
+    @product.songs.build
+  end
+
   def index
-   @products =Product.all
+  	@products =Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
   end
 
   def show
   end
-end
 
-private
-  def products_params
-    params.require(:product).permit(:artist, :cd_title, :image, :notax_price, :label, :genre, :stock )
+  def edit
+
   end
+
+  def update
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:artist, :cd_title, :image, :notax_price, :label, :genre, :stock,
+                                                         discs_attributes: [:id, :disc_num, :_destroy,
+                                                          songs_attributes:[:id,:track_num, :_destroy]] )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :users
   resources :cart_items
   resources :products
+  resources :labels
+  resources :artists
 
   namespace :admin do
     resources :products
