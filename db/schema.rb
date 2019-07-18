cd54c4a4a17c6f08a9fa7b544fc67a8aa61a2565
nishi <24yuta24@gmail.com>
2019-07-18 17:50:35 +0900

    [画像投稿]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_064331) do
+ActiveRecord::Schema.define(version: 2019_07_17_061240) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -63,13 +63,6 @@ ActiveRecord::Schema.define(version: 2019_07_18_064331) do
 
   create_table "labels", force: :cascade do |t|
     t.string "label"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "product_images", force: :cascade do |t|
-    t.text "image_id"
-    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
