322444ad136929d4ad2cb15274e7bd1ebc3980a6
新井　雄介 <ay088055@gmail.com>
2019-07-21 14:33:38 +0900

    kaminari2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,7 @@ class ProductsController < ApplicationController
   end
 
   def index
-  	@products = Product.all.order(created_at: :desc)
-    @products = Product.page(params[:page]).per(5)
+  	@products = Product.page(params[:page]).per(5)
   end
 
   def create
