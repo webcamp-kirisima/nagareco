6cc91036a74776c31ffa20b4550a5a049f0ed775
倉片肇 <kurakatahajime@kurakatahajimenoMacBook-Air.local>
2019-07-21 13:59:07 +0900

    商品登録機能 バリデーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,12 @@ class Product < ApplicationRecord
 	has_many :product_images, dependent: :destroy
 	attachment :image
 	accepts_nested_attributes_for :discs, allow_destroy: true
+
+	validates :cd_title, presence: true
+	validates :artist, presence: true
+	validates :image, presence: true
+	validates :notax_price, presence: true
+	validates :label, presence: true
+	validates :genre, presence: true
+	validates :stock, presence: true, numericality: { :greater_than => 0 }
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
 	belongs_to :disc
+
+	validates :track_num, presence: true
+	validates :song, presence: true
 end
