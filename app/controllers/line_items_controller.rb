9608cfbe77cc7b744b054fd5cf7fa0216ad40223
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-07-25 17:05:14 +0900

    add-T

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,30 +1,23 @@
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /line_items
-  # GET /line_items.json
+
   def index
     @line_items = LineItem.all
   end
 
-  # GET /line_items/1
-  # GET /line_items/1.json
+
   def show
   end
 
-  # GET /line_items/new
   def new
     @line_item = LineItem.new
   end
 
-  # GET /line_items/1/edit
   def edit
   end
 
 
-
-  # POST /line_items
-  # POST /line_items.json
   def create
 
 
@@ -53,8 +46,6 @@ class LineItemsController < ApplicationController
   end
 end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
@@ -67,8 +58,6 @@ end
     end
   end
 
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
  def destroy
     @line_item.destroy
 
@@ -80,12 +69,12 @@ end
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_line_item
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def line_item_params
       params.require(:line_item).permit(:product_id, :cart_id, :quantity)
     end
