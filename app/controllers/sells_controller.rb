62e5effba4c07e1eaa385f49fb23ae899f64b2da
nyarlarla <24yuta24@gmail.com>
2019-07-25 09:23:12 +0000

    A

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -1,90 +1,89 @@
 class SellsController < ApplicationController
 
-  before_action :set_sell, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  # GET /products
+  # GET /products.json
   def index
-    @sells = Sell.all
+    @products = Product.all
   end
 
+  # GET /products/1
+  # GET /products/1.json
   def show
   end
 
+  # GET /products/new
   def new
-    @cart = current_cart
-    if @cart.line_items.empty?
-       redirect_to products_path, notice: 'カートは空です。'
-      return
-    end
-
-    @sell = Sell.new
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @sell }
-    end
+  @cart = current_cart
+  if @cart.line_items.empty?
+    redirect_to products_path, notice: 'カートは空です。'
+    return
   end
 
-  def create_conf
-    @cart = current_cart
-    @sell = Sell.new(sell_params)
+  @product = Product.new
 
-    respond_to do |format|
-      format.html
-      format.json { render json: @sell }
-    end
+  respond_to do |format|
+    format.html
+    format.json { render json: @product }
+  end
   end
 
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products
+  # POST /products.json
   def create
-    @sell = Sell.new(sell_params)
+    @product = Product.new(product_params)
+    @product.add_items(current_cart)
 
     respond_to do |format|
-      if @sell.save
-          Cart.destroy(session[:cart_id])
-          session[:cart_id] = nil
-          format.html { redirect_to sells_finish_path}
-          format.json { render json: @sell, status: :created, location: @sell }
+      if @product.save
+  Cart.destroy(session[:cart_id])
+  session[:cart_id] = nil
+        format.html { redirect_to products_path, notice: 'ご注文ありがとうございました。' }
+        format.json { render json: @product, status: :created, location: @product }
       else
-          format.html { redirect_to products_path }
-          format.json { render json: @sell.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
-  end
+end
 
-
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
   def update
     respond_to do |format|
-      if @sell.update(sell_params)
-        format.html { redirect_to @sell, notice: 'sell was successfully updated.' }
-        format.json { render :show, status: :ok, location: @sell }
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
       else
-        format.html { redirect_to products_path }
-        format.json { render json: @sell.errors, status: :unprocessable_entity }
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  # DELETE /products/1
+  # DELETE /products/1.json
   def destroy
-    @sell.destroy
+    @product.destroy
     respond_to do |format|
-      format.html { redirect_to sells_url, notice: 'sell was successfully destroyed.' }
+      format.html { redirect_to products_path, notice: 'product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  def finish
-  end
-
-
-
-
   private
-
-  def set_sell
-      @sell = Sell.find(params[:id])
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
     end
 
-  def sell_params
-      params.require(:sell).permit( :user_id, :pay, :ship_address, :ship_tel, :ship_code, :ship_name, :total)
-  end
-
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:sip_name, :ship_address, :ship_tel, :pay)
+    end
 end
