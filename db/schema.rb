70c186def6237949a7d8e1795ebdb6c82f26c9b4
新井　雄介 <ay088055@gmail.com>
2019-07-29 11:16:25 +0900

    product-show10

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_07_27_045450) do
 
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
